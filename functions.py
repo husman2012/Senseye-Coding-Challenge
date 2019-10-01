74018da639590494b52073e33b0301e9be4d35de
husman2012 <husman2012@utexas.edu>
2019-10-01 15:41:30 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -7,6 +7,12 @@ import os
 #Metrics variables
 
 def acc (input, target):
+    '''
+    Metrics function to assess accuracy of model
+    input: input, target
+    returns: accuracy in float of all pixel values
+    '''
+    
     target = target.squeeze(1)
     mask = target != global_vars.void_code
     return (input.argmax(dim = 1)[mask] == target[mask]).float().mean()
@@ -15,22 +21,31 @@ def acc (input, target):
 def create_data(path, bs, path_lbl, path_img):
     """
     Function to create DataBunch object from FastAI
-
+    inputs:
+        path, WindowsPathObject
+        bs, batch_size int
+        path_lbl, WindowsPathObject
+        path_img, WindowsPathObject
+    returns:
+        data, DataBunch Object from FastAI
     """
-    #Set paths to masks and imgs
     
+    #Create lists for image and label files
     fnames = get_image_files(path_img)
     lbl_names = get_image_files(path_lbl)
 
-    #function required by FastAI to get paths to each image in masks
+    #function required to get paths to each image in masks
     get_y_fn = lambda x: path_lbl/f'{x.stem}.png'
 
     #Function to open respective masks based on original image
     img_f = fnames[10]
     mask = open_mask(get_y_fn(img_f))
-
+    
+    #Get size of mask and reduce
     src_size = np.array(mask.shape[1:])
     size = src_size//2
+    
+    #create validation list
     create_segments(path_img)
 
     #Create DataBunch
@@ -39,16 +54,14 @@ def create_data(path, bs, path_lbl, path_img):
 
     return data
 
-
-
-#My own function to randomly create validation list ("valid.txt")
+#Function to randomly create validation list ("valid.txt")
 def create_segments(path_to_img, pct = 0.10):
     """
     Function to create valid.txt to randomly select validation set.
     10% is the default value
     
     arguments: WindowsPath Object to img folder
-    returns: None
+    returns: New file named valid.txt into path directory
     """
     
     mask_list = os.listdir(path_to_img)
