5f7d276826018cbccdd7205d635db2f2b240741b
husman2012 <husman2012@utexas.edu>
2019-10-01 15:32:55 -0500

    Update create_masks.py

--- create_masks.py
+++ create_masks.py
@@ -5,7 +5,7 @@ from fastai.vision import *
 import numpy as np
 import os
 
-#Produce final mask and display result, save into new folder named test_folder
+#Produce final masks, save into folder specified
 def acc (input, target):
         target = target.squeeze(1)
         mask = target != global_vars.void_code
@@ -20,18 +20,26 @@ parser.add_argument('--save_dir', default = 'output_folder')
 args = parser.parse_args()
 global_vars.init(args.filename)
 
+#Create path variables
 path_img = global_vars.path/args.pred_dir
 path_lbl = global_vars.path/args.save_dir
+
+#Create list of image file names
 fnames = get_image_files(path_img)
+
+#Load learner
 learn = load_learner(global_vars.path)
+
+#Function to get names of images to later save masks with the same name
 get_y_fn = lambda x: path_lbl/f'{x.stem}.png'
+
 n_masks = len(fnames)
 for image in fnames:
-  
     img = open_image(image)
     y = learn.predict(img)[0]
     y.save((get_y_fn(image)))
 
+#Condition check if user didn't put any masks in correct folder
 if n_masks == 0:
     print("No images found in {}".format(args.pred_dir))
 print("{} masks saved in {}".format(n_masks, args.save_dir))
